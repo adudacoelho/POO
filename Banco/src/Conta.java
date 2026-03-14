624b4500a934fb0bb50820bd3bc5851d89a9b605
Eduarda Nascimento Coelho <dudacoelho13@hotmail.com>
2026-03-14 19:44:54 -0300

    Adicionando método Cheque Especial

--- Banco/src/Conta.java
+++ Banco/src/Conta.java
@@ -3,27 +3,32 @@ public class Conta {
     Pessoa titular;
     double saldo, limite;
 
-    double disponivel (){
+
+    double disponivel(){
         return this.saldo + this.limite;
     }
+
 
     void extrato(){
         System.out.println("\nEXTRATO DA CONTA \n");
         System.out.println("Número:" + this.numero);
-        System.out.println("Nome:" + this.titular.nome);
-        System.out.println("Disponível: R$" + this.disponivel()+ "\n");
+        System.out.println("Titular:" + this.titular.cpf);
+        System.out.println("Disponível: R$" + this.disponivel() + "\n");
     }
+
 
     void depositar(double v){
         this.saldo += v;
-        System.out.println("Deposito realizado com sucesso!" + "\n");
+        System.out.println("Depósito realizado com sucesso!" + "\n");
     }
+
 
     boolean sacar(double v){
         if (this.disponivel() >= v){
             this.saldo -= v;
-            System.out.println("Saque realizado com sucesso, na conta: " + this.numero + "no valor de " + v + "\n");
+            System.out.println("Saque realizado com sucesso na conta " + this.numero + "Novo saldo: " + this.saldo + "\n");
             return true;
+
 
         }
         else{
@@ -32,19 +37,31 @@ public class Conta {
         }
     }
 
+
     boolean transf (double v, Conta dest){
         if(this.sacar(v)){
             dest.depositar(v);
-            System.out.println("Transferencia de " + v + " da conta " + this.numero + " para a conta " + dest.numero + " foi realizado com sucesso." + "\n");
+            System.out.println("Transferência de " + v + " da conta " + this.numero + " para a conta " + dest.numero + " foi realizado com sucesso." + "\n");
             return true;
         }
         else {
             System.out.println("ERRO: Não foi possível transferir " + v + " da conta " + this.numero + " para a conta " + dest.numero + ".  Valor disponível: " + this.disponivel() + "\n");
             return false;
 
+
+        }
+    }
+
+
+    void chequeEspecial(double juro){
+        if (this.saldo < 0){
+            this.saldo = this.saldo + (this.saldo * (juro/100));
+
+
         }
     }
 
 
 }
+
 
--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -2,55 +2,71 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    // Criando um objeto da classe Pessoa
-    Pessoa p1 = new Pessoa ();
-    p1.nome = " Maria ";
-    p1.idade = 18;
-    p1.sexo = 'F';
-    p1.cpf = "123.456.789-00";
+        // Criando um objeto da classe Pessoa
+        Pessoa p1 = new Pessoa ();
+        p1.nome = " Maria ";
+        p1.idade = 18;
+        p1.sexo = 'F';
+        p1.cpf = "123.456.789-00";
 
-    //Criando outro objeto da classe Pessoa
-    Pessoa p2 = new Pessoa();
-    p2.nome = " João ";
-    p2.idade = 22;
-    p2.sexo = 'M';
-    p2.cpf = "234.567.890-00";
 
-    //Criando um objeto da classe Conta
-    Conta c1 = new Conta();
-    c1.numero = " 1234-5 ";
-    c1.titular = p1;
-    c1.saldo = 100.0;
-    c1.limite = 200.0;
+        //Criando outro objeto da classe Pessoa
+        Pessoa p2 = new Pessoa();
+        p2.nome = " João ";
+        p2.idade = 22;
+        p2.sexo = 'M';
+        p2.cpf = "234.567.890-00";
 
-    //Criando outro objeto da classe Conta
-    Conta c2 = new Conta();
-    c2.numero = " 2345-6 ";
-    c2.titular = p2;
-    c2.saldo = 150.0;
-    c2.limite = 200.0;
 
-    c1.extrato();
-    c2.extrato();
+        //Criando um objeto da classe Conta
+        Conta c1 = new Conta();
+        c1.numero = " 1234-5 ";
+        c1.titular = p1;
+        c1.saldo = 100.0;
+        c1.limite = 200.0;
 
-    c1.sacar(150);
-    c1.transf(100, c2);
-    c1.sacar(100);
-    c1.depositar(100);
-    c1.transf(200, c2);
 
-    //Executando o metodo que imprime o extrato
-    System.out.println("\nEXTRATO DA CONTA \n");
+        //Criando outro objeto da classe Conta
+        Conta c2 = new Conta();
+        c2.numero = " 2345-6 ";
+        c2.titular = p2;
+        c2.saldo = 150.0;
+        c2.limite = 200.0;
 
-    System.out.println("Conta: " + c1.numero);
-    System.out.println("Titular: " + c1.titular.cpf);
-    System.out.println("Saldo da conta p/ saque: " + c1.saldo);
 
-    System.out.println("\nEXTRATO DA CONTA\n ");
+        c1.extrato();
+        c2.extrato();
 
-    System.out.println("Conta: " + c2.numero);
-    System.out.println("Titular: " + c2.titular.cpf);
-    System.out.println("Saldo da conta p/ saque: " + c2.saldo);
+
+        c1.sacar(150);
+        c1.transf(100, c2);
+        c1.sacar(100);
+        c1.depositar(100);
+        c1.transf(200, c2);
+
+
+        //Executando o metodo que imprime o extrato
+        System.out.println("\nEXTRATO DA CONTA \n");
+
+
+        System.out.println("Conta: " + c1.numero);
+        System.out.println("Titular: " + c1.titular.cpf);
+        System.out.println("Saldo da conta p/ saque: " + c1.saldo);
+
+
+        System.out.println("\nEXTRATO DA CONTA\n ");
+
+
+        System.out.println("Conta: " + c2.numero);
+        System.out.println("Titular: " + c2.titular.cpf);
+        System.out.println("Saldo da conta p/ saque: " + c2.saldo + "\n");
+
+
+        for(int i = 1; i <= 120; i++) {
+            c1.chequeEspecial(0.5);
+            System.out.println("Saldo após " + i + " dia(s): " + c1.saldo);
+        }
+
 
     }
 }
